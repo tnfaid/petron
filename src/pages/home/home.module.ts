8e5fccfbbf69daffea804a06b66a2ed4d340928d
tnfaid <nurfaidahtitik@gmail.com>
2018-08-08 00:17:18 +0700

    done menu tab icon

--- src/pages/home/home.module.ts
+++ src/pages/home/home.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { HomePage } from './home';
 
+
 @NgModule({
   declarations: [
     HomePage,
@@ -9,5 +10,8 @@ import { HomePage } from './home';
   imports: [
     IonicPageModule.forChild(HomePage),
   ],
+  exports: [
+    HomePage
+  ]
 })
 export class HomePageModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,8 +1,8 @@
 import { ApiProvider } from './../../providers/api/api';
-import { Component } from '@angular/core';
-import { IonicPage, NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, Refresher, List, ItemSliding, ToastController } from 'ionic-angular';
 import { Observable } from 'rxjs/Observable';//api
-
+import { AppState } from '../../app/app.global';
 /**
  * Generated class for the HomePage page.
  *
@@ -10,12 +10,15 @@ import { Observable } from 'rxjs/Observable';//api
  * Ionic pages and navigation.
  */
 
+
 @IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html',
 })
 export class HomePage {
+  @ViewChild('beritaList', { read: List }) beritaList: List;
+  openMenu = false;
   //------------------------- TAMPILAN GAMBAR HOME --------------------------------------//
   cards1: any;
   cards2: any;
@@ -24,7 +27,8 @@ export class HomePage {
 
   //------------------------- TAMPILAN BERITA HOME --------------------------------------//
   beritaku: Observable<any>;
-  constructor(public navCtrl: NavController, public apiProvider: ApiProvider) {
+  constructor(public navCtrl: NavController, public apiProvider: ApiProvider,
+   public toastCtrl : ToastController, public global: AppState) {
     this.beritaku = this.apiProvider.getBerita();
 
     this.beritaku
@@ -33,7 +37,71 @@ export class HomePage {
       })
   }
 
+  IonViewDidLoad(){
+     this.updateBerita();
+  }
+
+  updateBerita(){
+    // Close any open sliding items when the berita updates
+    this.beritaList && this.beritaList.closeSlidingItems();
+
+    this.apiProvider.getBerita();
+  }
+
   openDetails(berita) {
     this.navCtrl.push('BeritaDetailPage', { berita: berita });
   }
+
+  doRefresh(refresher: Refresher) {
+    this.beritaku = this.apiProvider.getBerita();
+
+      // simulate a network request that would take longer
+      // than just pulling from out local json file
+      setTimeout(() => {
+        refresher.complete();
+
+        const toast = this.toastCtrl.create({
+          message: 'Sessions have been updated.',
+          duration: 3000
+        });
+        toast.present();
+    }, 1000);
+  };
+
+  changeTheme(theme) {
+    this.global.set('theme', theme);
+    console.log(theme);
+  }
+
+  togglePopupMenu() {
+    return this.openMenu = !this.openMenu;
+  }
+
+  goToReportase() {
+    this.navCtrl.push('BeritaReportasePage');
+  }
+
+  goToPengumuman() {
+    this.navCtrl.push('PengumumanPage');
+  }
+
+  goToSiaran() {
+    this.navCtrl.push('SiaranPersPage');
+  }
+
+  goToArtikel() {
+    this.navCtrl.push('ArtikelPage');
+  }
+
+  goToHelp() {
+    alert('Help clicked.');
+    this.togglePopupMenu();
+  }
+
+  goToLaporan() {
+    this.navCtrl.push('LaporanPage');
+  }
+
+
 }
+
